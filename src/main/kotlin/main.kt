c16a6b31583873d12cb742f6973faf549149af3e
Dmitry Vinogradov <dmitrvino@gmail.com>
2022-06-30 17:33:13 +0300

    Changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,11 @@
+var amount: Double = 35.00
+const val percentOfCommission = 0.075
+const val minCommission = 35.00
 fun main() {
-   val amount: Double = 100.00
-    val percentOfCommission = 0.075
-    if(amount >= 35) {
-        var commission: Double = amount*percentOfCommission
-        println("Your commission: $commission rubles")
-    } else println("Your commission 35 rubles")
+    println("Your commission: ${isAmountMoreThan35(amount)} rubles")
+}
+fun isAmountMoreThan35 (amount: Double): Double {
+    return if (amount > minCommission) {
+        amount * percentOfCommission
+    } else minCommission
 }
